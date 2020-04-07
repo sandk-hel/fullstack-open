ca8d3cc2711f7dc4288b70f3c4b5bd7759514516
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-07 15:02:38 +0300

    Completed step 4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,9 +7,10 @@ const Header = (props) => (
 const Part = (props) => (
   <p>{props.title} {props.exercises}</p>
 )
-const Total = (props) => ( 
-  <p>Number of exercises {props.exercises}</p>
-)
+const Total = (props) => {
+  const allExercises = props.parts.reduce((a, b) => a + b.exercises, 0)
+  return <p>Number of exercises {allExercises}</p>
+}
 const Content = (props) => (
   props.parts.map(part => 
     <Part title={part.name} exercises={part.exercises} />
@@ -19,26 +20,26 @@ const Content = (props) => (
 
 const App = () =>  {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const parts = [part1, part2, part3]
-  const total = parts.reduce((a, b) => a + b.exercises, 0)
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }, 
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    }, 
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
       <Content parts={parts}/>
-      <Total exercises={total} />
+      <Total parts={parts} />
     </div>
   )
 }
