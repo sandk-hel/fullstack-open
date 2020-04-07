150a23c763c523edff118ed58407b79b9c0b56db
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-07 14:56:46 +0300

    Completed Step 3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) =>  <h1>{props.course}</h1>
-const Total = (props) => <p>Number of exercises {props.exercises}</p>
-const Part = (props) => <p>{props.title} {props.exercises}</p>
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+const Part = (props) => (
+  <p>{props.title} {props.exercises}</p>
+)
+const Total = (props) => ( 
+  <p>Number of exercises {props.exercises}</p>
+)
+const Content = (props) => (
+  props.parts.map(part => 
+    <Part title={part.name} exercises={part.exercises} />
+  )
+)
+
 
 const App = () =>  {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  
-  const Content = () => {
-    return (
-      <div>
-        <Part title={part1} exercises={exercises1} />
-        <Part title={part2} exercises={exercises2} />
-        <Part title={part3} exercises={exercises3} />
-      </div>
-    )
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
   }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const parts = [part1, part2, part3]
+  const total = parts.reduce((a, b) => a + b.exercises, 0)
 
   return (
     <div>
       <Header course={course}/>
-      <Content />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts}/>
+      <Total exercises={total} />
     </div>
   )
 }
