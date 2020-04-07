d75ecf5e6c3f6999363925367d3aad7ba1930c41
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-07 14:17:26 +0300

    Finished assignment 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Header = (props) =>  <h1>{props.course}</h1>
 const Total = (props) => <p>Number of exercises {props.exercises}</p>
+const Part = (props) => <p>{props.title} {props.exercises}</p>
 
 const App = () =>  {
   const course = 'Half Stack application development'
@@ -12,18 +13,16 @@ const App = () =>  {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
-  const Content = () => <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+  
+  const Content = () => {
+    return (
+      <div>
+        <Part title={part1} exercises={exercises1} />
+        <Part title={part2} exercises={exercises2} />
+        <Part title={part3} exercises={exercises3} />
+      </div>
+    )
+  }
 
   return (
     <div>
