6b5b4454c05f1a820cc6bc48f2bb5a40f7e2972b
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-10 14:27:02 +0300

    Refactor Statistics into a separate component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,6 +2,27 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Statistics = ({ good, bad, neutral }) => {
+  const all = good + neutral + bad
+
+  const positivePercent = () => `${good / all * 100} %`
+  const average = () => {
+     return (good * 1 + neutral * 0 + bad * -1) / all
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average()}</p>
+      <p>positive {positivePercent()}</p>
+    </div>
+  )
+}
+
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
     {text}
@@ -14,37 +35,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const getAll = () => good + neutral + bad
-
-  const getPositivePercent = () => {
-    const all = getAll()
-    if (all > 0) {
-      return `${good / getAll() * 100} %` 
-    }
-    return '0 %'
-  }
-
-  const getAverage = () => {
-    const all = getAll()
-    if (all > 0) {
-      return (good * 1 + neutral * 0 + bad * -1) / getAll()
-    }
-    return 0
-  }
-
   return (
     <div>
       <h1>Give feedback</h1>
       <Button onClick = { () => setGood(good + 1) } text="good" />
       <Button onClick = { () => setNeutral(neutral + 1) } text="neutral" />
       <Button onClick = { () => setBad(bad + 1) } text="bad" />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {getAll()}</p>
-      <p>average {getAverage()}</p>
-      <p>positive {getPositivePercent()}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
