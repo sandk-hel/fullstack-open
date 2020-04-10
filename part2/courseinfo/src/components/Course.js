b4f9698f22c79179fa2de0fef13413b9786f2a92
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-10 22:54:46 +0300

    Course contents Exercise 2.4

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 let Course = ({ course }) => (
     <>
         <Header course={course.name}/>
         <Content parts={course.parts}/>
+        <Total course={course} />
     </>
 )
 
