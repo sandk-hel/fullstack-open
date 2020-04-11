25f94efc5d0d46646dc1841b09ce1a4aca3aba54
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-11 10:54:30 +0300

    The Phonebook Step5 Assignment 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,67 +1,54 @@
 import React, { useState } from 'react';
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
+  const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
-  const [ searchText, setSearchText ] = useState('')
-  const [ newPerson, setNewPerson ] = useState({name: '', number: ''})
+  const [searchText, setSearchText] = useState('')
+  const [newPerson, setNewPerson] = useState({ name: '', number: '' })
 
-  const personsToShow = searchText.length === 0 
+  const personsToShow = searchText.length === 0
     ? [...persons]
     : persons.filter(person => person.name.toLowerCase().includes(searchText.toLowerCase()))
 
   const handleNameChange = (event) => {
-    setNewPerson({...newPerson, name: event.target.value})
+    setNewPerson({ ...newPerson, name: event.target.value })
   }
 
   const handlePhoneChange = (event) => {
-    setNewPerson({...newPerson, number: event.target.value})
+    setNewPerson({ ...newPerson, number: event.target.value })
   }
 
   const addNewPerson = (event) => {
     event.preventDefault()
     const containsName = persons.some(person => person.name === newPerson.name)
-    if(containsName) {
+    if (containsName) {
       alert(`${newPerson.name} is already added to phonebook`)
-      setNewPerson({name: '', number: ''})
+      setNewPerson({ name: '', number: '' })
       return
     }
-    setNewPerson({name: '', number: ''})
+    setNewPerson({ name: '', number: '' })
     setPersons([...persons, newPerson])
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with<input value={searchText} onChange={ event => setSearchText(event.target.value) } />
-      </div>
+      <Filter searchText={searchText} onChange={(event) => setSearchText(event.target.value)} />
       <h2>add a new </h2>
-      <form onSubmit={addNewPerson}>
-        <div>
-          name: 
-          <input value={newPerson.name} 
-                  onChange={handleNameChange}
-                  />
-          
-        </div>
-        <div>
-          number: 
-          <input value={newPerson.number} 
-                  onChange={handlePhoneChange}
-                  />
-          
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm newPerson={newPerson} 
+                  addNewPerson={addNewPerson} 
+                  handleNameChange={handleNameChange}
+                  handlePhoneChange={handlePhoneChange} />
+
       <h2>Numbers</h2>
-      {personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      <Persons persons={personsToShow} />
       {/* <div>debug: {newName}</div> */}
     </div>
   )
