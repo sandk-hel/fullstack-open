9e47aa4e3ffec2bf889a2beee32dff9fc749a270
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-11 03:35:34 +0300

    Phonebook step1 Assignment 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const handleChange = event => {
+    setNewName(event.target.value)
+  }
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    const newPerson = {name: newName}
+    setNewName('')
+    setPersons([...persons, newPerson])
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: 
+          <input value={newName} 
+                  onChange={handleChange}
+                  />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {/* <div>debug: {newName}</div> */}
+    </div>
+  )
+}
+
+export default App
