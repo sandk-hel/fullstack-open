18d2dd734bd47ae05bbc0598bee8af1f52c32c7c
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-22 01:02:27 +0300

    6.11 Better anecdotes, step9 - Create two new action creators showNotification and hideNotification - Add action reducers accordingly - Trigger action creators when upvote is done and hide after 5 secs

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
+import { showNotification, hideNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes.sort((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
+
+  const upvote = (anecdote) => {
+    dispatch(addVote(anecdote.id))
+
+    const message = `You upvoted \`${anecdote.content}\``
+    dispatch(showNotification(message))
+    setTimeout(() => dispatch(hideNotification()), 5000)
+  }
+
   return anecdotes.map(anecdote =>
     <div key={anecdote.id}>
       <div>
@@ -12,7 +22,7 @@ const AnecdoteList = () => {
       </div>
       <div>
         has {anecdote.votes}
-        <button onClick={() => dispatch(addVote(anecdote.id))}>vote</button>
+        <button onClick={() => upvote(anecdote)}>vote</button>
       </div>
     </div>
   )
--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -2,10 +2,25 @@ const initialNotification = 'Welcome to anecdotes!'
 
 const notificationReducer = (state = initialNotification, action) => {
   switch (action.type) {
-    case 'NOTIFICATION':
+    case 'SHOW_NOTIFICATION':
       return action.message
+    case 'HIDE_NOTIFICATION':
+      return null
     default:
       return state
+  }
+}
+
+export const showNotification = (message) => {
+  return {
+    type: 'SHOW_NOTIFICATION',
+    message
+  }
+}
+
+export const hideNotification = () => {
+  return {
+    type: 'HIDE_NOTIFICATION'
   }
 }
 
