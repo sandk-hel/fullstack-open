777b7f278a72b94a79bb357f4e73772c94136a33
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-04-22 00:24:05 +0300

    6.9 Better anecdotes, step7 - Add redux-devtools-extension npm package - Move store to its own file store.js

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const store = createStore(
+  anecdoteReducer,
+  composeWithDevTools()
+)
+
+export default store
