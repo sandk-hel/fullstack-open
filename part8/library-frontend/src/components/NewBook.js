b7bd580abcfe918f049ff723f097f6a4c4496628
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-05-05 15:32:47 +0300

    8.10: Adding a book Implement a possibility to add new books

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_BOOKS, ADD_BOOK, ALL_AUTHORS } from  '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
-  const [author, setAuhtor] = useState('')
+  const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  
+  const [ createBook ] = useMutation(ADD_BOOK, {
+    refetchQueries: [
+      { query: ALL_AUTHORS },
+      { query: ALL_BOOKS }
+    ]
+  })
 
   if (!props.show) {
     return null
@@ -14,11 +23,13 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
     
-    console.log('add book...')
+    createBook({
+      variables: { title, published: parseInt(published), author, genres  }
+    })
 
     setTitle('')
     setPublished('')
-    setAuhtor('')
+    setAuthor('')
     setGenres([])
     setGenre('')
   }
@@ -42,7 +53,7 @@ const NewBook = (props) => {
           author
           <input
             value={author}
-            onChange={({ target }) => setAuhtor(target.value)}
+            onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -15,6 +15,17 @@ query {
     title
     author
     published
+    id
   }
 }
+`
+export const ADD_BOOK = gql`
+  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title,  author: $author, published: $published, genres: $genres) {
+      title
+      author
+      published
+      id
+    }
+  }
 `
