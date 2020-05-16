f413cfc15db8ab05e4fe6fbb2b49a2ff37fda089
sandeep koirala <sandeep.koirala@helsinki.fi>
2020-05-16 14:13:55 +0300

    9.20: patientor, step5 - Extend a patient's page in the frontend to list the date, description and diagnose codes of the patient's entries.

--- part9/patientor/src/components/PatientDescription.tsx
+++ part9/patientor/src/components/PatientDescription.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Icon, SemanticICONS } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -9,6 +9,7 @@ import { useStateValue, addPatientDetail } from '../state';
 const PatientDescription: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const [{ patientsDetail }, dispatch] = useStateValue();
+  const [error, setError] = useState<string>();
 
   const patient = patientsDetail[id];
   const genderIcon = (): SemanticICONS => {
@@ -20,34 +21,60 @@ const PatientDescription: React.FC = () => {
       return "venus"
     }
 
-    if  (patient.gender === Gender.Female) {
+    if (patient.gender === Gender.Female) {
       return "mercury"
     }
 
     return "neuter"
   }
 
+  const showEntries = () => {
+    if (!patient) {
+      return null;
+    }
+
+    return patient.entries.map(entry => {
+      return <div>
+          <p><i>{entry.date} {entry.description}</i></p>
+          <ul>
+            {entry.diagnosisCodes?.map(code => <li>{code}</li>)}
+          </ul>
+        </div>
+    })
+  }
+
   useEffect(() => {
     const fetchPatient = async () => {
-      const patient = await axios.get<PatientDetail>(
-        `${apiBaseUrl}/patients/${id}`
-      );
-      dispatch(addPatientDetail(patient.data))
+      try {
+        const patient = await axios.get<PatientDetail>(
+          `${apiBaseUrl}/patients/${id}`
+        )
+        dispatch(addPatientDetail(patient.data))
+
+      } catch (error) {
+        setError(error.message);
+      }
     }
     if (!patient) {
       fetchPatient()
     }
   }, [patient, dispatch, id]);
 
+  if (error) {
+    return <h3 style={{color: 'red'}}>User could not be found.</h3>;
+  }
+
   if (patient) {
     return <div>
-      <h2>{patient.name} <Icon name={genderIcon()} /></h2> 
+      <h2>{patient.name} <Icon name={genderIcon()} /></h2>
       <p>
         ssn: {patient.ssn} <br />
         occupation: {patient.occupation}
       </p>
+      {showEntries()}
     </div>
   }
+
   return <h3>Patient loading</h3>;
 }
 
--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -10,9 +10,44 @@ export enum Gender {
   Other = "other"
 }
 
-export interface Entry {
-
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  specialist: string;
+  date: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: { date: string; criteria: string };
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string }
+}
+
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthCareEntry
 
 interface BasePatient {
   id: string;
@@ -21,8 +56,8 @@ interface BasePatient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
-  entry: Entry[]
+  entries: Entry[]
 }
 
-export type Patient = Omit<BasePatient, 'entry'> 
+export type Patient = Omit<BasePatient, 'entries'> 
 export type PatientDetail = BasePatient
